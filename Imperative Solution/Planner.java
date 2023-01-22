6349621903b1fd7ee4c8cd49ab8d8914f509f0f3
Enrik <enrik-rushiti@hotmail.com>
2023-01-21 21:18:31 -0700

    added generateSchedule and outputSchedule. Need testing

--- Imperative Solution/Planner.java
+++ Imperative Solution/Planner.java
@@ -1,23 +1,17 @@
-import java.io.File;  // Import the File class
-import java.io.FileNotFoundException;  // Import this class to handle errors
-import java.util.Scanner; // Import the Scanner class to read text files
 import java.io.*;
+import java.util.Scanner;
 
 public class Planner {
-
     public static void main(String[] args) {
 
-
         try {
-            Scanner input = new Scanner (new File("Test.txt"));
+            Scanner input = new Scanner(new File("test.txt"));
             input.useDelimiter("-|\n|hrs|mins|V");
-            Activity[] activities = new Activity[0]; //set to 0 initially
-            double currentTime = 8.30;
+            Activity[] activities = new Activity[0]; // set to 0 initially
+            double currentTime = 8.50;
             double endTime = 22.00; /* millitary time */
 
-
-
-            while(input.hasNext()) { //begin parsing input file for data
+            while (input.hasNext()) { // begin parsing input file for data
 
                 int id = input.nextInt();
                 String name = input.next();
@@ -27,35 +21,32 @@ public class Planner {
                 boolean allowed = true;
                 input.nextLine();
 
-                Activity newActivity = new Activity(name,duration,id,startRange,endRange, allowed,0,0);
-                activities = addActivity(activities, newActivity); //update array with new activty
+                Activity newActivity = new Activity(name, duration, id, startRange, endRange, allowed, 0, 0);
+                activities = addActivity(activities, newActivity); // update array with new activty
             }
 
-
             System.out.print("\n");
-     
-               for(int i =0; i <= activities.length-1; i++) {
-                   System.out.print(activities[i].name);
-                   System.out.print("\n");
-                   System.out.print(activities[i].duration);
-                   System.out.print("\n");
-                   System.out.print(activities[i].id);
-                   System.out.print("\n");
-                   System.out.print(activities[i].startRange);
-                   System.out.print("\n");
-                   System.out.print(activities[i].endRange);
-                   System.out.print("\n");
-                   System.out.print("\n");
 
+            /*
+            for (int i = 0; i <= activities.length - 1; i++) {
+                System.out.print(activities[i].name);
+                System.out.print("\n");
+                System.out.print(activities[i].duration);
+                System.out.print("\n");
+                System.out.print(activities[i].id);
+                System.out.print("\n");
+                System.out.print(activities[i].startRange);
+                System.out.print("\n");
+                System.out.print(activities[i].endRange);
+                System.out.print("\n");
+                System.out.print("\n");
 
+            }
+            */
 
-               }
+            generateSchedule(activities, currentTime);
+            checkAllowance(activities, currentTime);
 
-               checkAllowance(activities, currentTime);
-
-       
-
-           
             input.close();
 
         } catch (FileNotFoundException e) {
@@ -63,6 +54,18 @@ public class Planner {
             e.printStackTrace();
         }
 
+    }
+
+    private static void checkAllowance(Activity[] activities, double currentTime) {
+        /*
+         * first check if the activity starts before the selected start-time within
+         * planner, 8:30 am
+         */
+        for (int i = 0; i <= activities.length - 1; i++) {
+            if (activities[i].startRange < currentTime) {
+                activities[i].allowed = false; /* non valid activity, starts before allowed time */
+            }
+        }
     }
 
     private static Activity[] addActivity(Activity[] activities, Activity activityToAdd) {
@@ -73,16 +76,62 @@ public class Planner {
         return newActivities;
     }
 
-    private static int solution (Activity[] activities, double currentTime) {
+    public static void generateSchedule(Activity[] activities, double currentTime) {
 
-        return 0;
+        for (int i = 0; i < activities.length; i++) {
+            if (activities[i].duration > (activities[i].endRange - activities[i].startRange)) {
+                System.out.println("Activity " + activities[i].name
+                        + " not included in the schedule because the duration is greater than the time range.");
+                continue;
+            }
+            for (int j = i + 1; j < activities.length; j++) {
+                Activity a1 = activities[i];
+                Activity a2 = activities[j];
+                if (a1.startRange == a2.startRange) {
+                    double a1End = a1.startRange + a1.duration;
+                    double a2End = a2.startRange + a2.duration;
+                    if (a1End > a2.endRange) {
+                        Activity temp = activities[i];
+                        activities[i] = activities[j];
+                        activities[j] = temp;
+                    } else if (a2End > a1.endRange) {
+                        Activity temp = activities[j];
+                        activities[j] = activities[i];
+                        activities[i] = temp;
+                    }
+                }
+            }
+
+            outputSchedule(activities);
+        }
     }
 
-    public static void checkAllowance(Activity[] activities, double currentTime) {
-        /* first check if the activity starts before the selected start-time within planner, 8:30 am*/
-        for (int i = 0; i <= activities.length-1; i++) {
-            if (activities[i].startRange < currentTime) {
-                activities[i].allowed = false; /* non valid activity, starts before allowed time*/
+    private static void outputSchedule(Activity[] activities) {
+        for (int i = 0; i < activities.length; i++) {
+            if (activities[i].startRange < 8.5 || activities[i].endRange > 22.0) {
+                System.out.println("Activity " + activities[i].name
+                        + " not included in the schedule because it starts before 8.5 or ends after 22.0");
+                continue;
+            }
+            if (activities[i].duration > (activities[i].endRange - activities[i].startRange)) {
+                System.out.println("Activity " + activities[i].name
+                        + " not included in the schedule because the duration is greater than the time range");
+                continue;
+            }
+            double current = activities[i].startRange;
+            for (int j = i + 1; j < activities.length; j++) {
+                if (activities[i].startRange == activities[j].startRange) {
+                    if (activities[i].duration > activities[j].duration) {
+                        activities[j].startRange = activities[i].startRange + activities[i].duration;
+                    }
+                }
+            }
+            while (current < activities[i].endRange) {
+                System.out.println(current + " - " + activities[i].endRange + ": " + activities[i].name);
+                current += activities[i].duration;
+                if (current > activities[i].endRange) {
+                    break;
+                }
             }
         }
     }
@@ -105,9 +154,8 @@ public class Planner {
         return timeString;
     }
 
-
     // convert standard time to military time
-    public static double convertTime(String time) {
+    public static double convertMilitary(String time) {
         double timeDouble = 0;
         int hours = Integer.parseInt(time.substring(0, 2));
         int minutes = Integer.parseInt(time.substring(3, 5));
@@ -118,28 +166,35 @@ public class Planner {
         return timeDouble;
     }
 
-    public static void printSchedule(Activity[] activities) // this method takes in a array of activities and outputs it to the console.
-    {
-        for(Activity actvity : activities) // iterates each activity in the act
-        {
-            outputFormatter(actvity);
-        }
-    }
-    public static void outputFormatter(Activity activity) // prints out the activity for that time slot 
-    {
-
-        system.out.println("    " + "-------------------------------------------------------");
-        system.out.println("    " + "|");                                                 
-        system.out.println("    " + "|");                                                   
-        system.out.println("    " + "|"+ " Time: " + convertStandard(activity.actualStart) + "  -  "  + convertStandard(activity.actualEnd) );
-        system.out.println("    " + "|");
-        system.out.println("    " + "|" + " Task: " + actvity.name);
-        system.out.println("    " + "|");
-        system.out.println("    " + "|");
-        system.out.println("    " + "--------------------------------------------------------");
-        
-
-    }
-
-
-}/*end of class*/
+    /*
+     * NIMRAT'S CODE
+     * public static void printSchedule(Activity[] activities) // this method takes
+     * in a array of activities and outputs it to the console.
+     * {
+     * for(Activity actvity : activities) // iterates each activity in the act
+     * {
+     * outputFormatter(actvity);
+     * }
+     * }
+     * public static void outputFormatter(Activity activity) // prints out the
+     * activity for that time slot
+     * {
+     * 
+     * system.out.println("    " +
+     * "-------------------------------------------------------");
+     * system.out.println("    " + "|");
+     * system.out.println("    " + "|");
+     * system.out.println("    " + "|"+ " Time: " +
+     * convertStandard(activity.actualStart) + "  -  " +
+     * convertStandard(activity.actualEnd) );
+     * system.out.println("    " + "|");
+     * system.out.println("    " + "|" + " Task: " + actvity.name);
+     * system.out.println("    " + "|");
+     * system.out.println("    " + "|");
+     * system.out.println("    " +
+     * "--------------------------------------------------------");
+     * 
+     * 
+     * }
+     */
+}
