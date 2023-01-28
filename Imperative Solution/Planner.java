f4accc9b5d521be76715c4a45218d4f21ea2f702
Enrik <enrik-rushiti@hotmail.com>
2023-01-28 16:10:04 -0700

    standard time work we smile but one more fix needed

--- Imperative Solution/Planner.java
+++ Imperative Solution/Planner.java
@@ -9,7 +9,7 @@ public class Planner {
             input.useDelimiter("-|\n");
             Activity[] activities = new Activity[0]; // set to 0 initially
             double minimumTime = 8.30;
-            double maximumTime = 22.00; /* millitary time */
+            double maximumTime = 22.00;
 
             while (input.hasNext()) { // begin parsing input file for data
                 String name = input.next();
@@ -19,8 +19,8 @@ public class Planner {
                 boolean allowed = true;
                 input.nextLine();
 
-                startRange = convertTimeRange((int) startRange); // convert start time to decimal
-                endRange = convertTimeRange((int) endRange); // convert end time to decimal
+                startRange = convertMilitaryToDecimal((int) startRange); // convert start time to decimal
+                endRange = convertMilitaryToDecimal((int) endRange); // convert end time to decimal
                 double duration = getDurationInHours(durationInStr); // convert duration to hours
 
                 Activity newActivity = new Activity(name, duration, startRange, endRange, allowed, 0, 0);
@@ -29,24 +29,10 @@ public class Planner {
 
             System.out.print("\n");
 
-            generateSchedule(activities, minimumTime, maximumTime);
+            // create a new array of activities that are allowed to be scheduled
+            Activity[] allowedActivities = createAllowedActivitiesOnlyArray(activities, minimumTime, maximumTime);
 
-            /*
-             * for (int i = 0; i <= activities.length - 1; i++) {
-             * System.out.print(activities[i].name);
-             * System.out.print("\n");
-             * System.out.print(activities[i].duration);
-             * System.out.print("\n");
-             * System.out.print(activities[i].id);
-             * System.out.print("\n");
-             * System.out.print(activities[i].startRange);
-             * System.out.print("\n");
-             * System.out.print(activities[i].endRange);
-             * System.out.print("\n");
-             * System.out.print("\n");
-             * 
-             * }
-             */
+            generateSchedule(allowedActivities);
 
             input.close();
 
@@ -57,9 +43,25 @@ public class Planner {
 
     }
 
-    public static void generateSchedule(Activity[] activities, double minimumTime, double maximumTime) {
-        // Sort the activities by their start time
-        java.util.Arrays.sort(activities, (a1, a2) -> {
+    /* 
+     * createAllowedActivitiesOnlyArray()
+     * 
+     * This method creates a new array of activities that are allowed to be scheduled
+     * and removes the activities that are not allowed to be scheduled.
+     * It will also sort the activities by their start time.
+    */
+    public static Activity[] createAllowedActivitiesOnlyArray(Activity[] activities, double minimumTime, double maximumTime) {
+        // check if the activities are allowed to be scheduled
+        checkAllowance(activities, minimumTime, maximumTime);
+        
+        Activity[] allowedActivities = new Activity[0];
+        for (int i = 0; i < activities.length; i++) {
+            if (activities[i].allowed) {
+                allowedActivities = addActivity(allowedActivities, activities[i]);
+            }
+        }
+        // sort the activities by their start time
+        java.util.Arrays.sort(allowedActivities, (a1, a2) -> {
             if (a1.getStartRange() == a2.getStartRange()) {
                 return Double.compare(a1.getEndRange(), a2.getEndRange());
             } else {
@@ -67,9 +69,10 @@ public class Planner {
             }
         });
 
-        // Check the activities for any issues
-        checkAllowance(activities, minimumTime, maximumTime);
+        return allowedActivities;
+    }
 
+    public static void generateSchedule(Activity[] activities) {
         // Initialize the schedule
         Activity[] schedule = new Activity[activities.length];
         double scheduledTime = activities[0].getStartRange();
@@ -115,8 +118,11 @@ public class Planner {
     public static void printSchedule(Activity[] activities) {
         for (int i = 0; i < activities.length; i++) {
             if (activities[i].allowed) {
-                System.out.println(activities[i].getName() + ": " + activities[i].actualStart + " - "
-                        + activities[i].actualEnd);
+                int start = convertDecimalToMilitary(activities[i].actualStart);
+                String startStr = convertMilitaryToStandard(start);
+                int end = convertDecimalToMilitary(activities[i].actualEnd);
+                String endStr = convertMilitaryToStandard(end);
+                System.out.println(activities[i].getName() + ": " + startStr + " - " + endStr);
             }
         }
     }
@@ -129,12 +135,23 @@ public class Planner {
     }
 
     // convert the values of the time ranges from decimal to military time
-    public static int convertDecimalToMilitary(int time) {
-        int hours = time / 100;
-        int mins = time % 100;
-        return hours * 100 + mins;
+    public static int convertDecimalToMilitary(double time) {
+        int hours = (int) time;
+        int minutes = (int) Math.round((time - hours) * 60);
+        if (hours <= 12) {
+            if (hours == 0) {
+                hours = 12;
+            }
+            return (hours * 100) + minutes;
+        } else {
+            hours -= 12;
+            if (hours == 0) {
+                hours = 12;
+            }
+            return (hours * 100) + minutes + 1200;
+        }
     }
-
+    
     // convert the duration into hours if it is in minutes
     public static double getDurationInHours(String duration) {
         String[] arr = duration.split(" ");
@@ -157,10 +174,25 @@ public class Planner {
      */
     private static void checkAllowance(Activity[] activities, double minimumTime, double maximumTime) {
         for (int i = 0; i <= activities.length - 1; i++) {
+            /* 
+            if the activity starts before the selected start-time within planner, or
+            after the selected end-time within planner, then it is not allowed to be
+            added to the schedule 
+            */
             if (activities[i].startRange < minimumTime || activities[i].endRange > maximumTime) {
                 activities[i].allowed = false;
             }
+            /*
+             * if the duration of the activity is greater than the time range, then it is
+             * not allowed to be added to the schedule
+            */
             if (activities[i].duration > (activities[i].endRange - activities[i].startRange)) {
+                activities[i].allowed = false;
+            }
+            /*
+             * if start range and end range are the same, then it is not allowed to be added
+            */
+            if (activities[i].startRange == activities[i].endRange) {
                 activities[i].allowed = false;
             }
         }
@@ -176,26 +208,22 @@ public class Planner {
     }
 
     // convert military time to standard time
-    public static String convertStandard(double time) {
+    public static String convertMilitaryToStandard(double time) {
         String timeString = "";
-        int hours = (int) time;
-        int minutes = (int) ((time - hours) * 60);
-        if (hours > 12) {
-            hours -= 12;
-            timeString = hours + ":" + minutes + "pm";
-        } else if (hours == 12) {
-            timeString = hours + ":" + minutes + "pm";
-        } else if (hours == 0) {
-            hours = 12;
-            timeString = hours + ":" + minutes + "am";
-        } else {
-            timeString = hours + ":" + minutes + "am";
-        }
+        int hours = (int) time/100;
+        int minutes = (int) time % 100;
+        String am_pm = hours >= 12 ? "pm" : "am";
+        hours = hours % 12;
+        hours = hours == 0 ? 12 : hours; 
+        String formattedMinutes = String.format("%02d", minutes);
+        timeString = hours + ":" + formattedMinutes + am_pm;
         return timeString;
     }
+    
+    
 
     // convert standard time to military time
-    public static double convertMilitary(String time) {
+    public static double convertStandardToMilitary(String time) {
         double timeDouble = 0;
         int hours = Integer.parseInt(time.substring(0, 2));
         int minutes = Integer.parseInt(time.substring(3, 5));
