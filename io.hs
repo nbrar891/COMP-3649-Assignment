39aa035e2c99c52f26e8bee6bd2090304867efcb
Pelvain Dhanda <100167525+pdhan780@users.noreply.github.com>
2023-04-02 21:13:35 -0600

    Create io.hs
    io.hs-handles io section,
    compile with "ghc io.hs"
    then run:   ./io test.txt

---
+++ io.hs
@@ -0,0 +1,44 @@
+import Data.List.Split (splitOn)
+import System.Environment (getArgs)
+import Data.Char (isDigit)
+
+data Activity = Activity {
+  name :: String,
+  startRange :: Int,
+  endRange :: Int,
+  duration :: Int,
+  allowed :: Bool,
+  actualStart :: Int,
+  actualEnd :: Int
+}
+
+readActivities :: FilePath -> IO [Activity]
+readActivities filename = do
+  contents <- readFile filename
+  return (map parseActivity (lines contents))
+
+parseActivity :: String -> Activity
+parseActivity input =
+  let fields = splitOn "-" input
+      name = fields !! 0
+      startRange = read (fields !! 1)
+      endRange = read (fields !! 2)
+      duration = read (takeWhile isDigit (fields !! 3))
+  in Activity name startRange endRange duration False 0 0
+
+printActivity :: Int -> Activity -> IO ()
+printActivity index activity =
+  putStrLn (show index ++ "." ++ name activity ++ "-" ++ show (actualStart activity) ++ "-" ++ show (actualEnd activity) ++ "-" ++ show (duration activity))
+
+printActivities :: [Activity] -> IO ()
+printActivities activities =
+  mapM_ (uncurry printActivity) (zip [1..] activities)
+
+main :: IO ()
+main = do
+  args <- getArgs
+  case args of
+    [filename] -> do
+      activities <- readActivities filename
+      printActivities activities
+    _ -> putStrLn "Usage: programname inputfilename"
