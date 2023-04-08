9cbdc2fa293f390cb3989c4900ad94a4e94f2240
Enrik <99112423+FrikiEnriki@users.noreply.github.com>
2023-04-07 21:27:10 -0600

    Add files via upload

--- HaskellCurrent/scheduling.hs
+++ HaskellCurrent/scheduling.hs
@@ -2,7 +2,10 @@ module Scheduling where
 
 import Types (Activity(..))
 import Helpers (addMinutes)
-import Data.List (find)
+import Data.List (find, sortBy, sortOn)
+import Data.Maybe (fromJust)
+import MyIO (printActivities)
+
 -- import Myio (printActivities)
 
 
@@ -12,75 +15,58 @@ generateStartTimes activity =
   generateStartTimesHelper (startRange activity) (endRange activity) (duration activity)
   where
     generateStartTimesHelper start end duration
-      | addMinutes start duration <= end = start : generateStartTimesHelper (addMinutes start 5) end duration
+      | addMinutes start duration <= end = start : generateStartTimesHelper (addMinutes start 1) end duration
       | otherwise = []
 
--- Assigns start times to activities
-assignStartTime :: Activity -> Activity
-assignStartTime activity@(Activity name startRange endRange duration allowed _ _) =
-  let startTimes = generateStartTimes activity
-      assignStartTimeHelper :: [Int] -> Activity
-      assignStartTimeHelper [] = activity -- No suitable start time found, return original activity
-      assignStartTimeHelper (start:rest) =
-        let actualEnd = addMinutes start duration
-        in if actualEnd <= endRange
-            then activity { actualStart = start, actualEnd = actualEnd } -- Found a suitable start time
-            else assignStartTimeHelper rest -- Continue with the next start time option
-  in assignStartTimeHelper startTimes
+-- Chooses a value from the start time options list, based on if there is a conflict with a previous activity
+chooseStartTime :: [Int] -> [Activity] -> [Activity] -> Int -> Maybe Int
+chooseStartTime [] _ _ _ = Nothing
+chooseStartTime (startTime:xs) previousActivities remainingActivities duration =
+  let endTime = addMinutes startTime duration
+      ifConflictFound = conflictsWith startTime endTime (previousActivities ++ remainingActivities)
+  in if ifConflictFound then chooseStartTime xs previousActivities remainingActivities duration else Just startTime
 
+
+
+conflictsWith :: Int -> Int -> [Activity] -> Bool
+conflictsWith start end activities =
+  let conflictingActivities = filter (conflictFound start end) activities
+  in not $ null conflictingActivities
 
 -- Check if two activities have a conflict
-conflictFound :: Activity -> Activity -> Bool
-conflictFound activity1 activity2 =
-  let start1 = actualStart activity1
-      end1 = actualEnd activity1
-      start2 = actualStart activity2
+conflictFound :: Int -> Int -> Activity -> Bool
+conflictFound start1 end1 activity2 =
+  let start2 = actualStart activity2
       end2 = actualEnd activity2
   in start1 < end2 && end1 > start2
 
--- Schedule an activity by assigning start time and end time
-scheduleActivity :: Activity -> [Activity] -> Activity
-scheduleActivity activity previousActivities =
-  let startTimes = generateStartTimes activity
-      suitableStartTimes = filter (\start -> not $ any (\prev -> conflictFound activity prev) previousActivities) startTimes
-      assignStartTimeHelper :: [Int] -> Activity
-      assignStartTimeHelper [] = activity -- No suitable start time found, return original activity
-      assignStartTimeHelper (start:rest) =
-        let actualEnd = addMinutes start (duration activity)
-            updatedActivity = activity { actualStart = start, actualEnd = actualEnd }
-            updatedPreviousActivities = updatedActivity : previousActivities
-        in if null suitableStartTimes
-            then activity -- No suitable start time found, return original activity
-            else if any (\prev -> conflictFound updatedActivity prev) previousActivities
-              then assignStartTimeHelper rest -- Continue with the next start time option
-              else updatedActivity -- Found a suitable start time
-  in assignStartTimeHelper suitableStartTimes
 
--- Schedule activities recursively
-scheduleHelper :: [Activity] -> [Activity] -> [Activity]
-scheduleHelper [] scheduledActivities = scheduledActivities
-scheduleHelper (activity:rest) previousActivities =
-  let scheduledActivity = scheduleActivity activity previousActivities
-      updatedPreviousActivities = scheduledActivity : previousActivities
-      updatedScheduledActivities = scheduledActivity : scheduleHelper rest updatedPreviousActivities
-  in updatedScheduledActivities
-
--- Main scheduling function
 schedule :: [Activity] -> [Activity]
-schedule activities = scheduleHelper activities []
+schedule activities = head $ scheduleActivities activities []
 
+scheduleActivities :: [Activity] -> [Activity] -> [[Activity]]
+scheduleActivities [] scheduledActivities = [scheduledActivities]
+scheduleActivities (currentActivity:remainingActivities) scheduledActivities =
+  [ scheduledActivity : restOfSchedule
+  | startTime <- generateStartTimes currentActivity
+  , not $ conflictsWith startTime (addMinutes startTime (duration currentActivity)) scheduledActivities
+  , let scheduledActivity = currentActivity { actualStart = startTime, actualEnd = addMinutes startTime (duration currentActivity) }
+  , restOfSchedule <- scheduleActivities remainingActivities (scheduledActivities ++ [scheduledActivity])
+  ]
 
 
 
 -- testing
 main :: IO ()
 main = do
-  let breakfast = Activity "breakfast" 830 930 30 True 0 0
-      lunch = Activity "gym" 830 1130 120 True 0 0
-      dinner = Activity "lunch" 1200 1300 30 True 0 0
+  let gym = Activity "gym" 830 1130 120 True 0 0
+      lunch = Activity "lunch" 1200 1300 30 True 0 0
+      homework = Activity "homework" 1200 1930 120 True 0 0
+      breakfast = Activity "breakfast" 830 850 20 True 0 0
+      ponderLife = Activity "ponder life" 830 1630 120 True 0 0
+      kickboxing = Activity "kickboxing" 1200 1500 120 True 0 0
 
-      -- Schedule activities
-      scheduledActivities = schedule [breakfast, lunch, dinner]
+      scheduledActivities = schedule [gym, lunch, homework, breakfast, ponderLife, kickboxing]
+      
   putStrLn "Scheduled Activities: "
-  mapM_ print scheduledActivities
-
+  printActivities scheduledActivities
